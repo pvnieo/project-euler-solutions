93b75656b7a0aff80d9790e3daf38b71278cd089
pvnieo <sohaib.attaiki@gmail.com>
2018-09-28 19:33:12 +0200

    add solutions

---
+++ 14.py
@@ -0,0 +1,26 @@
+
+def next_collatz(n):
+    if n % 2 == 0:
+        return n // 2
+    else:
+        return 3 * n + 1
+
+def collatz_length(n):
+    s = 1
+    while n != 1:
+        n = next_collatz(n)
+        s += 1
+    return s
+
+M = 1000000
+max_length, arg = 1, 1
+for i in range(1, M+1):
+    print(i, "\r", end="")
+    length = collatz_length(i)
+    if length > max_length:
+        max_length = length
+        arg = i
+
+print()
+print(arg)
+
--- 15.py
+++ 15.py
@@ -1,21 +1,10 @@
 
+
 def lattice(n,m):
-    # if n == 1 :
-    #     return 1
-    # elif n == 13 and m == 13:
-    #     return 2704156
-    # else:
-    #     total = 0
-    #     for i in range(1, m+1):
-    #         total += lattice(n-1, m-i+1)
-    #     return total
     lat = [[1 for i in range(m)] for i in range(n)]
     for i in range(n-2,-1,-1):
         for j in range(m-2,-1, -1):
             lat[i][j] = lat[i+1][j] +lat[i][j+1]
-
-
-
     return lat[0][0]
 
 print(lattice(21,21))
