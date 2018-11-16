3ff191738e7078f8e07a4396f498f13f350f7fa9
pvnieo <sohaib.attaiki@gmail.com>
2018-11-16 21:14:23 +0100

    add problem

---
+++ python version/52.py
@@ -0,0 +1,15 @@
+
+def is_multi_has_same_digits(x):
+    base = sorted(list(map(int, str(x))))
+    for i in range(2, 7):
+        if base != sorted(list(map(int, str(x * i)))):
+            return False
+    return True
+
+x = 10
+
+while(not is_multi_has_same_digits(x)):
+    print(x, "\r", end="")
+    x += 1
+
+print(x)
